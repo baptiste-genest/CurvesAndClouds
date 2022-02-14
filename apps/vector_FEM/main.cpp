89d25298fb21cf13c7225c2f72312d9c08d80023
Eulerson314 <baptistegenest@gmail.com>
2022-02-14 17:54:16 +0100

    push

--- apps/vector_FEM/main.cpp
+++ apps/vector_FEM/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     PlotLayer* L = F->add_grid_layer(r,r,false);
     //auto M = std::make_shared<Mesh2>(mesh_generation::LloydRelaxation(convexPrimitive::Rectangle(3,1.5).subdivide(5),100));
     auto P = convexPrimitive::Rectangle(3,1).subdivide(5);
-    auto M = std::make_shared<Mesh2>(mesh_generation::FromBoundaryMesh({P},0.05,P.getShapePredicate()));
+    auto M = std::make_shared<Mesh2>(mesh_generation::FromBoundaryMesh(P.getPointCloud(),0.05,P.getShapePredicate()));
     /*
     auto S = algo::FEM::LinearElasticity(M,{0,1,2,3,4,5},[](const vec&) {
         return linear_utils::vec2(0,-0.01);
